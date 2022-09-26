9fbcc1d169ef38e38a27d3b12997bf3010a85afa
TofaelKabir <tofaelkabir@gmail.com>
2022-09-25 22:47:10 -0400

    code to the Day3 branch

--- src/main/java/BaseUtil/BaseClass.java
+++ src/main/java/BaseUtil/BaseClass.java
@@ -15,22 +15,22 @@ public class BaseClass {
 	
 	@BeforeMethod
 	public void setUp() {
+		/*
 		// System is a class and setProperty is a method
 		// 1st way, to show the location of chrome driver
 		// This is an absolute path
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tofael\\eclipse-workspace\\gov.portal.cms\\driver\\chromedriver.exe");
-		// WebDriver is an interface and ChromeDriver is a concrete class which implements WebDriver interface
-		driver = new ChromeDriver(); // This line will instantiate the Webdriver and chromedriver will be used
-		driver.manage().window().maximize(); // or fullscreen() method
+		*/
+		// new
+		// 2nd way, to show the location of chrome driver
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
+		
+		driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get("https://portal.cms.gov/portal");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-		// WebDriver will wait for 20 seconds to load the page and then it will through exception
-		// But if the loading is done before 20 seconds, it will go to next level
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // Learn from here, very very important interview question
-		// Here, WebDriver will wait for 20 seconds to find any web element under this class 
-		// and then it will through exception if web element is not found
-		// But if the web element is found before 20 seconds, it will go to next step, it means it will not wait for 20 seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); 
 		
 		homePage = new HomePage(driver);
 	}
